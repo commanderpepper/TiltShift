dfe12eaa37ede2f616aed521595514714285a200
commanderpepepr <ahmadhumza7@gmail.com>
2020-06-21 01:04:25 -0400

    Using a BroadcastChannel for button event.

--- app/src/main/java/com/commanderpepper/tiltshift/MainActivity.kt
+++ app/src/main/java/com/commanderpepper/tiltshift/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.ui.core.setContent
 import androidx.ui.foundation.Text
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
+import androidx.ui.layout.Row
 import androidx.ui.layout.size
 import androidx.ui.material.Button
 import androidx.ui.material.Surface
@@ -17,49 +18,73 @@ import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.commanderpepper.tiltshift.ui.AppTheme
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 
-val colors = listOf(Color.Red, Color.Green, Color.Blue)
+val colors = listOf(Color.Red, Color.Green, Color.Blue, Color.Magenta)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val channelEvents = Channel<Unit>()
-        
+        val channelEvents = BroadcastChannel<Unit>(1)
+        val channelEventsFlow = channelEvents.asFlow()
+
         setContent {
             AppTheme {
                 Column {
-                    ChangeColorButton(lifecycleScope){channelEvents.send(Unit)}
-                    Tile(lifecycleScope, colors, channelEvents.receiveAsFlow(), 0)
-                    Tile(lifecycleScope, colors, channelEvents.receiveAsFlow(), 1)
-                    Tile(lifecycleScope, colors, channelEvents.receiveAsFlow(), 2)
+                    ChangeColorButton(lifecycleScope) { channelEvents.send(Unit) }
+                    Row{
+                        repeat(16){
+                            ColumnOfTiles(lifecycleScope, channelEventsFlow)
+                        }
+                    }
                 }
             }
+        }
+    }
+
+
+}
+
+@Composable
+fun ColumnOfTiles(coroutineScope: CoroutineScope, channelEventsFlow: Flow<Unit>) {
+    Column {
+        repeat(16){
+            Tile(coroutineScope, colors, channelEventsFlow)
         }
     }
 }
 
 @Composable
-fun ChangeColorButton(coroutineScope: CoroutineScope, changeButton:  suspend () -> Unit){
+fun ChangeColorButton(coroutineScope: CoroutineScope, changeButton: suspend () -> Unit) {
     Button(onClick = { coroutineScope.launch { changeButton.invoke() } }) {
         Text(text = "Change the colors")
     }
 }
 
 @Composable
-fun Tile(coroutineScope: CoroutineScope, colors: List<Color>, colorEvent: Flow<Unit>, colorIndex: Int) {
-    val currentColorIndex = state {colorIndex}
+fun Tile(
+    coroutineScope: CoroutineScope,
+    colors: List<Color>,
+    colorEvent: Flow<Unit>,
+    colorIndex: Int = Random.nextInt(0, colors.size)
+) {
+    val currentColorIndex = state { colorIndex }
 
-    Surface(modifier = Modifier.size(64.dp), color = colors[currentColorIndex.value]) {
+    Surface(modifier = Modifier.size(24.dp), color = colors[currentColorIndex.value]) {
+        // Listen for color events.
         colorEvent.onEach {
-            currentColorIndex.value = when {
-                currentColorIndex.value < colors.size - 1 -> currentColorIndex.value + 1
-                else -> 0
-            }
+//            currentColorIndex.value = when {
+//                currentColorIndex.value < colors.size - 1 -> currentColorIndex.value + 1
+//                else -> 0
+//            }
+            currentColorIndex.value = Random.nextInt(0, colors.size)
         }.launchIn(coroutineScope)
     }
 
